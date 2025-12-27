13e2675c91979bfafce1725668e6fb43057f8a7f
chocoscoding <chocoscoding@gmail.com>
2025-12-27 19:54:51 +0100

    ..

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+})
 
 
 
